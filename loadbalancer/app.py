b0a90da9184385f5091d46e3b58fd00b3e0f5e6d
Khadaryussuf <salatkhadar@gmail.com>
2026-07-14 12:06:58 +0300

    Add Task 3: /rm endpoint

--- loadbalancer/app.py
+++ loadbalancer/app.py
@@ -50,7 +50,6 @@ def add_replicas():
             "status": "failure"
         }), 400
 
-    # Fill in any missing hostnames with randomly generated ones
     while len(hostnames) < n:
         hostnames.append(generate_hostname())
 
@@ -58,6 +57,53 @@ def add_replicas():
         replicas[next_server_id] = hostname
         hash_map.add_server(next_server_id)
         next_server_id += 1
+
+    return jsonify({
+        "message": {
+            "N": len(replicas),
+            "replicas": list(replicas.values())
+        },
+        "status": "successful"
+    }), 200
+
+
+@app.route("/rm", methods=["DELETE"])
+def remove_replicas():
+    payload = request.get_json(force=True, silent=True)
+    if payload is None or "n" not in payload:
+        return jsonify({
+            "message": "<Error> Invalid payload, expected JSON with 'n' field",
+            "status": "failure"
+        }), 400
+
+    n = payload["n"]
+    hostnames = payload.get("hostnames", [])
+
+    if len(hostnames) > n:
+        return jsonify({
+            "message": "<Error> Length of hostname list is more than removable instances",
+            "status": "failure"
+        }), 400
+
+    # Find server_ids matching the requested hostnames
+    ids_to_remove = []
+    for hostname in hostnames:
+        for server_id, existing_hostname in replicas.items():
+            if existing_hostname == hostname:
+                ids_to_remove.append(server_id)
+                break
+
+    # If fewer hostnames were specified than n, randomly pick more to remove
+    remaining_ids = [sid for sid in replicas.keys() if sid not in ids_to_remove]
+    while len(ids_to_remove) < n and remaining_ids:
+        chosen = random.choice(remaining_ids)
+        ids_to_remove.append(chosen)
+        remaining_ids.remove(chosen)
+
+    # Actually remove them
+    for server_id in ids_to_remove:
+        hash_map.remove_server(server_id)
+        del replicas[server_id]
 
     return jsonify({
         "message": {
